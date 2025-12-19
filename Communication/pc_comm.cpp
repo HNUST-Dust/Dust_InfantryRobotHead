b573736221a7c0bcd6ed26c74af18da0f8f2224c
NoneOfEver <noneofever@gmail.com>
2025-12-20 03:01:16 +0800

    上位机沟通修复

--- Communication/pc_comm.cpp
+++ Communication/pc_comm.cpp
@@ -18,7 +18,7 @@ void Class_PC_Comm::Send_Message()
 
 void Class_PC_Comm::RxCpltCallback()
 {
-    if (PC_Recv_Data.head[0] == 'S' && PC_Recv_Data.head[1] == 'P'){
+    if (bsp_usb_rx_buffer[0] == 'S' && bsp_usb_rx_buffer[1] == 'P'){
         g_recived_flag = 1;
         memcpy(&PC_Recv_Data,bsp_usb_rx_buffer,29 * sizeof(uint8_t));
     }
